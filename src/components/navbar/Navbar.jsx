20fa899e8f1230eb6c3cbc7b1cc21e9ae9623345
ma9da <dodymasry25@gmail.com>
2022-03-30 16:46:30 +0200

    added: navbar component

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import { logoWhite } from "../../assets/img/logo-white.png";
+function Navbar() {
+    return (
+        <header class="nav">
+            <section class="container flexContainer">
+                <figure class="navLogo">
+                    <img src={logoWhite} alt="logo" />
+                </figure>
+                <label for="active" class="toggleButton" id="openToggle">
+                    <i onclick="changeIcon(this)" class='bx bx-menu'></i>
+                </label>
+                <input type="checkbox" name="active" id="active">
+                    <nav id="navOverlay">
+                        <ul class="navLinks">
+                            <li><a href="/">01 ABOUT NATOUS</a></li>
+                            <li><a href="/">02 YOUR BENFITS</a></li>
+                            <li><a href="/">03 POPULAR TOURS</a></li>
+                            <li><a href="/">04 STORIES</a></li>
+                            <li><a href="/">05 BOOK NOW</a></li>
+                        </ul>
+                    </nav>
+            </section>
+        </header>
+    )
+}
+
+export default Navbar
