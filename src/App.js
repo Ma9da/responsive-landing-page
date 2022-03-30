9b4f0bd248b1e343b51acc0d529bd07a7f8f803f
ma9da <dodymasry25@gmail.com>
2022-03-30 17:44:43 +0200

    added: review to the app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Features from "./components/features/Features";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Popular from "./components/popular/Popular";
+import Review from "./components/review/Review";
 import Tour from "./components/tour/Tour";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Tour />
       <Features />
       <Popular/>
+      <Review/>
     </>
   );
 }
