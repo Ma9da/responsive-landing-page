f5f2e9ebc93994a90df109ae8d1f45de29538428
ma9da <dodymasry25@gmail.com>
2022-03-30 16:50:27 +0200

    added: navbar link to stylw sheet

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
-import { logoWhite } from "../../assets/img/logo-white.png";
+import  logoWhite  from "../../assets/img/logo-white.png";
+import "./Navbar.css"
 function Navbar() {
     return (
         <header class="nav">
@@ -9,16 +10,16 @@ function Navbar() {
                 <label for="active" class="toggleButton" id="openToggle">
                     <i onclick="changeIcon(this)" class='bx bx-menu'></i>
                 </label>
-                <input type="checkbox" name="active" id="active">
-                    <nav id="navOverlay">
-                        <ul class="navLinks">
-                            <li><a href="/">01 ABOUT NATOUS</a></li>
-                            <li><a href="/">02 YOUR BENFITS</a></li>
-                            <li><a href="/">03 POPULAR TOURS</a></li>
-                            <li><a href="/">04 STORIES</a></li>
-                            <li><a href="/">05 BOOK NOW</a></li>
-                        </ul>
-                    </nav>
+                <input type="checkbox" name="active" id="active" />
+                <nav id="navOverlay">
+                    <ul class="navLinks">
+                        <li><a href="/">01 ABOUT NATOUS</a></li>
+                        <li><a href="/">02 YOUR BENFITS</a></li>
+                        <li><a href="/">03 POPULAR TOURS</a></li>
+                        <li><a href="/">04 STORIES</a></li>
+                        <li><a href="/">05 BOOK NOW</a></li>
+                    </ul>
+                </nav>
             </section>
         </header>
     )
