82cc2b7427ba638f7fbe844c464f9470caddcf2e
ma9da <dodymasry25@gmail.com>
2022-03-30 17:33:38 +0200

    edit: class to className

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Features from "./components/features/Features";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Tour from "./components/tour/Tour";
@@ -5,9 +6,10 @@ import Tour from "./components/tour/Tour";
 function App() {
   return (
     <>
-    <Navbar/>
-    <Hero/>
-    <Tour/>
+      <Navbar />
+      <Hero />
+      <Tour />
+      <Features />
     </>
   );
 }
--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -2,41 +2,41 @@ import React from 'react'
 import "./features.css"
 function Features() {
     return (
-        <section class="features">
-            <div class="container flexContainer">
-                <div class="featuresWrapper">
+        <section className="features">
+            <div className="container flexContainer">
+                <div className="featuresWrapper">
                     {/* <!-- strat --> */}
-                    <div class="featureCard">
-                        <span class="featureIcon"> <box-icon name='globe' color='#777777' ></box-icon> </span>
-                        <h4 class="featureTitle">EXPLORE THE WORLD</h4>
-                        <p class="featureInfo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,
+                    <div className="featureCard">
+                        <span className="featureIcon"> <box-icon name='globe' color='#777777' ></box-icon> </span>
+                        <h4 className="featureTitle">EXPLORE THE WORLD</h4>
+                        <p className="featureInfo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,
                             ipsum
                             sapiente aspernatur.</p>
                     </div>
                     {/* <!-- end --> */}
                     {/* <!-- start  --> */}
-                    <div class="featureCard">
-                        <span class="featureIcon"> <box-icon name='compass' color='#777777' ></box-icon> </span>
-                        <h4 class="featureTitle">MEET NATURE</h4>
-                        <p class="featureInfo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,
+                    <div className="featureCard">
+                        <span className="featureIcon"> <box-icon name='compass' color='#777777' ></box-icon> </span>
+                        <h4 className="featureTitle">MEET NATURE</h4>
+                        <p className="featureInfo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,
                             ipsum
                             sapiente aspernatur.</p>
                     </div>
                     {/* <!-- end --> */}
                     {/* <!-- start  --> */}
-                    <div class="featureCard">
-                        <span class="featureIcon"><box-icon name='map-alt' color='#777777' ></box-icon></span>
-                        <h4 class="featureTitle">FIND YOUR WAY</h4>
-                        <p class="featureInfo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,
+                    <div className="featureCard">
+                        <span className="featureIcon"><box-icon name='map-alt' color='#777777' ></box-icon></span>
+                        <h4 className="featureTitle">FIND YOUR WAY</h4>
+                        <p className="featureInfo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,
                             ipsum
                             sapiente aspernatur.</p>
                     </div>
                     {/* <!-- end --> */}
                     {/* <!-- start  --> */}
-                    <div class="featureCard">
-                        <span class="featureIcon"> <box-icon name='heart' color='#777777' ></box-icon> </span>
-                        <h4 class="featureTitle">LIVE A HEALTHIER LIFE</h4>
-                        <p class="featureInfo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,
+                    <div className="featureCard">
+                        <span className="featureIcon"> <box-icon name='heart' color='#777777' ></box-icon> </span>
+                        <h4 className="featureTitle">LIVE A HEALTHIER LIFE</h4>
+                        <p className="featureInfo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam,
                             ipsum
                             sapiente aspernatur.</p>
                     </div>
