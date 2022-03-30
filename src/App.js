1fdccb19657b7aa5a92f5a6b5476af98f6b64a37
ma9da <dodymasry25@gmail.com>
2022-03-30 17:26:26 +0200

    added: tour to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import Tour from "./components/tour/Tour";
 
 function App() {
   return (
     <>
     <Navbar/>
     <Hero/>
+    <Tour/>
     </>
   );
 }
